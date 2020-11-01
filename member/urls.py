f13469f477263f34f8693654f487508ccd32fe78
Dacker Daw <dacker.daw@gmail.com>
2020-11-01 16:10:01 +0700

    crud member

--- member/urls.py
+++ member/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('members/', views.list_members, name='members'),
+    path('edit/<int:member_id>', views.edit_member, name='edit_member'),
+    path('delete/<int:member_id>', views.delete_member, name='delete_member'),
 ]
