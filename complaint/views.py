a592a7e065b3d38009af26224d8c2ad0e71049c7
Dacker Daw <dacker.daw@gmail.com>
2020-11-01 17:14:51 +0700

    update complaint

--- complaint/views.py
+++ complaint/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.urls import reverse
+from django.forms.models import model_to_dict
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import complaintMessageForm
 from .models import complaintMessage
 
@@ -20,5 +22,32 @@ def list_complaints(request):
     complaints = complaintMessage.objects.all()
     context = {
         'complaints': complaints,
+    }
+    return render(request, 'complaint/complaints.html', context=context)
+
+def edit_complaint(request, complaint_id):
+    if request.method == 'POST':
+        cur_complaint = complaintMessage.objects.get(pk=complaint_id)
+        form = complaintMessageForm(request.POST, instance=cur_complaint)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('complaints'))
+    else:
+        cur_complaint = complaintMessage.objects.get(pk=complaint_id)
+        fields = model_to_dict(cur_complaint)
+        form = complaintMessageForm(initial=fields, instance=cur_complaint)
+    context = {
+        'form': form,
+        'type': 'edit',
+    }
+    return render(request, 'complaint/message.html', context=context)
+
+def delete_complaint(request, complaint_id):
+    cur_complaint = complaint.objects.get(pk=complaint_id)
+    if request.method == 'POST':
+        cur_complaint.delete()
+        return HttpResponseRedirect(reverse('complaints'))
+    context = {
+        'complaint': cur_complaint
     }
     return render(request, 'complaint/complaints.html', context=context)
